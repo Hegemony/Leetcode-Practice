bfb892ac1fbd28aec671b5e2e655aab1beab8609
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-10-14 10:26:40 +0800

    Add files via upload

---
+++ 剑指 Offer/53-2. missingNumber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums) -> int:
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] == m:
+                i = m + 1
+            else:
+                j = m - 1
+        return i
