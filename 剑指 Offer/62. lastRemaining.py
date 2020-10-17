c97f29da682e8d26e3230d3d2322ca45c8ec0c17
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-10-17 09:40:46 +0800

    Add files via upload

---
+++ 剑指 Offer/62. lastRemaining.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lastRemaining(self, n: int, m: int) -> int:
+        a = list(range(n))
+        i = 0
+        while len(a) > 1:
+            i = (i + m - 1) % len(a)
+            a.pop(i)
+        return a[0]
