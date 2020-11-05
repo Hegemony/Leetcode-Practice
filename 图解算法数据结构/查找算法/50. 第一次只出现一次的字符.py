c95890406b81a5ce777de1b5239fc2ab5b7939b7
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-11-05 09:49:16 +0800

    Add files via upload

---
+++ 图解算法数据结构/查找算法/50. 第一次只出现一次的字符.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> str:
+        dic = {}
+
+        for i in range(len(s)):
+            dic[s[i]] = dic.get(s[i], 0) + 1
+        for i in range(len(s)):
+            if dic[s[i]] == 1:
+                return s[i]
+        return ' '
