40f3452cd19ddb360cc4aa9a79555d6240ffc21e
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-11-12 09:34:53 +0800

    Add files via upload

---
+++ 剑指 Offer/59-2. MaxQueue.py
@@ -0,0 +1,31 @@
+class MaxQueue:
+
+    def __init__(self):
+        self.res = []
+        self.flag = []
+    def max_value(self) -> int:
+        if self.flag:
+            return self.flag[0]
+        else:
+            return -1
+
+    def push_back(self, value: int) -> None:
+        self.res.append(value)
+        while self.flag and self.flag[-1] < value:
+            self.flag.pop()
+        self.flag.append(value)
+
+    def pop_front(self) -> int:
+        if len(self.res) != 0:
+            reult = self.res.pop(0)
+            if reult == self.flag[0]:
+                self.flag.pop(0)
+            return reult
+        else:
+            return -1
+
+# Your MaxQueue object will be instantiated and called as such:
+# obj = MaxQueue()
+# param_1 = obj.max_value()
+# obj.push_back(value)
+# param_3 = obj.pop_front()
