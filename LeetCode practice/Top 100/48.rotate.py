21cb4c59ebcc46afea133a5921a93f6aba9ab133
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-11-16 10:31:15 +0800

    Add files via upload

---
+++ LeetCode practice/Top 100/48.rotate.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotate(self, matrix) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        for i in range(m):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
+        return matrix
+
+
+print(Solution().rotate([
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16]
+]))
