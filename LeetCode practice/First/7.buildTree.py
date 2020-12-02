09367eb7371f6d43416c2591920b28256c3fc5e3
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2020-12-02 10:07:53 +0800

    Add files via upload

---
+++ LeetCode practice/First/7.buildTree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def helper(in_left, in_right):
+            if in_left > in_right:
+                return None
+            val = preorder.pop(0)
+            index = idx_map[val]
+
+            root = TreeNode(val)
+            root.left = helper(in_left, index - 1)
+            root.right = helper(index + 1, in_right)
+            return root
+
+        idx_map = {val: idx for idx, val in enumerate(inorder)}
+        return helper(0, len(inorder) - 1)
+
