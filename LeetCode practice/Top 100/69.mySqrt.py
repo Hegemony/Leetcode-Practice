d48c4deccc7e2bf4ec9195cd5257175ff18ed824
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2021-01-13 00:01:39 +0800

    Add files via upload

---
+++ LeetCode practice/Top 100/69.mySqrt.py
@@ -0,0 +1,7 @@
+import math
+class Solution:
+    def mySqrt(self, x):
+        if x == 0:
+            return 0
+        ans = int(math.exp(0.5 * math.log(x)))
+        return ans + 1 if (ans + 1) ** 2 <= x else ans
