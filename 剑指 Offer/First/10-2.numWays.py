5e1b9501cea7f645982b647ca6ac3e54e63a7ff5
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2021-02-24 22:21:02 +0800

    Add files via upload

---
+++ 剑指 Offer/First/10-2.numWays.py
@@ -0,0 +1,6 @@
+class Solution:
+    def numWays(self, n: int) -> int:
+        dp = [1 for i in range(n + 1)]
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 2] + dp[i - 1]
+        return dp[n] % 1000000007
