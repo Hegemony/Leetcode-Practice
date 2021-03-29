52c0ef99c810ba746c5ea828052122e22eab958d
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2021-03-29 10:10:49 +0800

    Add files via upload

---
+++ 剑指 Offer/First/58-2.reverseLeftWords.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        res = ""
+        for i in range(n, len(s)):
+            res += s[i]
+        for i in range(n):
+            res += s[i]
+        return res
