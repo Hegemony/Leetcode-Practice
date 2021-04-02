4ed2477dcb1da8fb6daa79d98175889ab4449d41
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2021-04-02 09:58:24 +0800

    Add files via upload

---
+++ 剑指 Offer/First/63.maxProfit.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+        if len(prices) == 0:
+            return 0
+        minn = float('inf')
+        dp = [0 for i in range(len(prices))]
+        for i in range(1, len(prices)):
+            minn = min(minn, prices[i - 1])
+            dp[i] = max(dp[i - 1], prices[i] - minn)
+        return max(dp)
+
+
+print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
