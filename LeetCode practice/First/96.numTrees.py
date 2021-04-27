737c2a88be04d05ad93a345f0ad29495950c48e8
Boa Hancock <38655976+Hegemony@users.noreply.github.com>
2021-04-27 11:10:25 +0800

    Add files via upload

--- LeetCode practice/First/96.numTrees.py
+++ LeetCode practice/First/96.numTrees.py
@@ -4,5 +4,9 @@ class Solution:
         dp[0], dp[1] = 1, 1
         for i in range(2, n + 1):
             for j in range(1, i + 1):
-                dp[i] += dp[j - 1] * dp[i - j]
+                dp[i] += dp[j - 1] * dp[i - j]  # 树左侧的数量 * 树右侧的数量
         return dp[n]
+        # C = 1
+        # for i in range(n):
+        #     C = C * 2 * (2 * i + 1) / (i + 2)
+        # return int(C)
